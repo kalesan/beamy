753a9294d5dd9c58fb846193e6c540e5a845ddea
Jarkko Kaleva <jarkko.kaleva@ee.oulu.fi>
2014-10-02 16:56:26 +0300

    Fixed in gainmod

--- marconi/gainmod.py
+++ marconi/gainmod.py
@@ -28,7 +28,7 @@ class Uniform1(object):
         self.K = K
         self.B = B
 
-        radius = kwargs.get('radius', 100)
+        self.radius = kwargs.get('radius', 100.)
         r_ref = kwargs.get('r_ref', 1.0)
 
         # Path loss exponents
@@ -36,38 +36,48 @@ class Uniform1(object):
         path_loss_exp_bs = kwargs.get('path_loss_exp_bs', 2.5)
         path_loss_exp_d2d = kwargs.get('path_loss_exp_d2d', 3.0)
 
-        SNR_edge = kwargs.get('SNR', 20)
+        # Distance of the D2D pairs
+        self.d2d_dist = kwargs.get('d2d_dist', 20)
+
+        SNR_edge = kwargs.get('SNR', 20.)
 
         angles = np.r_[0:K] * 2*np.pi/K
 
-        locs = radius * (np.cos(angles) + 1j*np.sin(angles))
+        # Mid-points
+        # locs = self.radius * (np.cos(angles) + 1j*np.sin(angles))
+        angl = np.arcsin((self.d2d_dist / 2) / self.radius)
+
+        coord_recv = self.radius * (np.cos(angles+angl) +
+                                    1j*np.sin(angles+angl))
+        coord_tran = self.radius * (np.cos(angles-angl) +
+                                    1j*np.sin(angles-angl))
 
         # dist_BS = [np.abs(locs[k]) for k in range(K)]
 
-        dist_UE = [[np.abs(locs[i]-locs[j])
-                    for i in range(K)] for j in range(K)]
+        # Transmitter distance from receivers j from i
+        dist_UE = np.zeros((K, K))
 
+        for (i, j) in product(range(K), range(K)):
+            dist_UE[i, j] = np.abs(coord_recv[i] - coord_tran[j])
+
+        # Gains
         self.gains = {}
         self.gains['D2D'] = np.zeros((K, K))
 
         for (i, j) in product(range(K), range(K)):
-            self.gains['D2D'][i, j] = (r_ref/dist_UE[i][j])**path_loss_exp_d2d
-
-            if i == j:
-                self.gains['D2D'][i, j] = 1
+            self.gains['D2D'][i, j] = (r_ref/dist_UE[i, j])**path_loss_exp_d2d
 
         self.gains['B2D'] = np.ones((K, 1))
         self.gains['D2B'] = np.ones((K, 1))
         for i in range(K):
-            self.gains['B2D'][i] = (r_ref/radius)**path_loss_exp_bs
+            self.gains['B2D'][i] = (r_ref/self.radius)**path_loss_exp_bs
 
         self.gains['D2B'] = self.gains['B2D']
 
         # Cell Edge SNR
+        self.SNR_ue_dB = SNR_edge + \
+            path_loss_exp_d2d * 10 * np.log10(self.d2d_dist)
 
-        self.SNR_dB = SNR_edge + path_loss_exp_bs*10*np.log10(radius)
+        SNR_lin = 10**(self.SNR_ue_dB/10) * K
 
-        SNR_lin = 10**(self.SNR_dB/10) / K
-
-        self.SNR_ue_dB = 10*np.log10(SNR_lin)
-
+        self.SNR_dB = 10*np.log10(SNR_lin)
