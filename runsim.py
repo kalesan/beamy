09cd584f570ad9534cea64535609f74ea62dceee
Jarkko Kaleva <jarkko.kaleva@gmail.com>
2022-08-10 14:41:51 +0300

    runsim.py: add more sim path options

--- runsim.py
+++ runsim.py
@@ -1,4 +1,22 @@
-import sys
+import os
+import argparse
 import subprocess
 
-subprocess.call(["python", sys.argv[1]])
+description = 'Run beamy simulation'
+parser = argparse.ArgumentParser(description=description)
+
+parser.add_argument('-p', '--path', help="Run simulation from specific file",
+                    default=None)
+
+parser.add_argument('-s', '--simulation', type=str,
+                    help="Run simulation from default sims/ folder",
+                    default=None)
+
+args = parser.parse_args()
+
+if args.simulation:
+    subprocess.call(["python", os.path.join('sims', args.simulation, 'run.py')])
+elif args.path:
+    subprocess.call(["python", args.path])
+else:
+    print("No simulation given")
