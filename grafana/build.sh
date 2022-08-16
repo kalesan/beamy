9d4684ca75f322ab889bdad53e35719a86f6b03f
Jarkko Kaleva <jarkko.kaleva@gmail.com>
2022-08-16 10:20:55 +0300

    Install Grafana plugin grafana-mqtt-datasource

--- grafana/build.sh
+++ grafana/build.sh
@@ -2,5 +2,5 @@
 
 docker build \
        --build-arg "GRAFANA_VERSION=9.0.6" \
-       --build-arg "GF_INSTALL_PLUGINS=grafana-simple-json-datasource,natel-plotly-panel,ae3e-plotly-panel" \
+       --build-arg "GF_INSTALL_PLUGINS=https://github.com/grafana/mqtt-datasource/archive/refs/heads/main.zip;grafana-simple-json-datasource,natel-plotly-panel,ae3e-plotly-panel" \
        -t grafana-beamy -f Dockerfile .
