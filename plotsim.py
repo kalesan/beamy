fe8ba10e95031bda2dc13b75506b169e56374b02
Jarkko Kaleva <jarkko.kaleva@gmail.com>
2022-08-17 15:02:53 +0300

    Initial plotsim.py draft

---
+++ plotsim.py
@@ -0,0 +1,17 @@
+import sys
+import numpy as np
+import pylab as plt
+
+dwmmse = np.load(sys.argv[1])
+#dsdp = np.load('sdp.npz')
+
+niters = dwmmse['R'].shape[0]
+
+plt.plot(range(niters), dwmmse['R'], 'k--', label='WMMSE')
+#plt.plot(range(niters), dsdp['R'], 'r-', label='SDP')
+
+legend = plt.legend(loc='lower right', shadow=True, fontsize='x-large')
+
+# legend.get_frame().set_facecolor('#00FFCC')
+
+plt.show()
