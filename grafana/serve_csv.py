82909f9b161a0e72b75ee4f50fcbdfd090b5d73a
Jarkko Kaleva <jarkko.kaleva@gmail.com>
2022-08-30 12:48:19 +0300

    [Frontend] Support CSV data

---
+++ grafana/serve_csv.py
@@ -0,0 +1,39 @@
+import sys
+import os
+import glob
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+HOST_NAME="0.0.0.0"
+PORT = 8008
+
+class PythonServer(SimpleHTTPRequestHandler):
+    """Python HTTP Server that handles GET and POST requests"""
+    def do_GET(self):
+        if self.path == '/':
+            dirs = [os.path.basename(os.path.normpath(d)) for d in glob.glob('./sims/*/')]
+            self.send_response(200, "OK")
+            self.end_headers()
+            self.wfile.write(bytes('\n'.join(['simulation'] + dirs), encoding='utf-8'))
+        else:
+            path = os.path.split(self.path)
+
+            if path[-1] == 'iteration':
+                path = os.path.join('./sims', './'+path[0], 'iteration.csv')
+                print("Serve", path)
+                with open(path) as f:
+                    self.send_response(200, "OK")
+                    self.end_headers()
+                    self.wfile.write(bytes(f.read(), 'utf-8'))
+
+if __name__ == "__main__":
+    server = HTTPServer((HOST_NAME, PORT), PythonServer)
+
+    print(f"Server started http://{HOST_NAME}:{PORT}")
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.server_close()
+        print("Server stopped succesfully")
+        sys.exit(0)
---
+++ servesims.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python ./grafana/serve_csv.py
