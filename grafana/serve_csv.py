44ac08793ce368c24dee66b0159dbe390f4621a2
Jarkko Kaleva <jarkko.kaleva@gmail.com>
2022-09-05 16:13:01 +0300

    [Frontend] support variable parameters

--- grafana/serve_csv.py
+++ grafana/serve_csv.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import glob
+import urllib
+import pandas as pd
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
@@ -18,18 +20,46 @@ class PythonServer(SimpleHTTPRequestHandler):
         else:
             path = os.path.split(self.path)
 
-            if path[-1] == 'iteration':
+            if path[-1].startswith('iteration'):
                 path = os.path.join('./sims', './'+path[0], 'iteration.csv')
-                with open(path) as f:
-                    self.send_response(200, "OK")
-                    self.end_headers()
-                    self.wfile.write(bytes(f.read(), 'utf-8'))
+                df = pd.read_csv(path)
+
+                query = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+
+                ret = df
+
+                if 'info' in query.keys():
+                    if query['info'] == 'BS':
+                        ret = df['BS'].unique()
+                    elif query['info'] == 'UE':
+                        ret = df['UE'].unique()
+                    elif query['info'] == 'Nr':
+                        ret = df['Nr'].unique()
+                    elif query['info'] == 'Nt':
+                        ret = df['Nt'].unique()
+                    elif query['info'] == 'SNR':
+                        ret = df['SNR'].unique()
+
+                if 'Nr' in query.keys():
+                    ret = ret[ret['Nr'] == int(query['Nr'][0])]
+                if 'Nt' in query.keys():
+                    ret = ret[ret['Nt'] == int(query['Nt'][0])]
+                if 'BS' in query.keys():
+                    ret = ret[ret['BS'] == int(query['BS'][0])]
+                if 'UE' in query.keys():
+                    ret = ret[ret['UE'] == int(query['UE'][0])]
+                if 'SNR' in query.keys():
+                    ret = ret[ret['SNR'] == int(query['SNR'][0])]
+
+                self.send_response(200, "OK")
+                self.end_headers()
+                self.wfile.write(bytes(ret.to_csv(), 'utf-8'))
             elif path[-1] == 'info':
                 path = os.path.join('./sims', './'+path[0], 'info.csv')
-                with open(path) as f:
-                    self.send_response(200, "OK")
-                    self.end_headers()
-                    self.wfile.write(bytes(f.read(), 'utf-8'))
+                df = pd.read_csv(path)
+                self.send_response(200, "OK")
+                self.end_headers()
+                self.wfile.write(bytes(df.to_csv(), 'utf-8'))
             else:
                 self.send_response_only(400, "Not found")
 
